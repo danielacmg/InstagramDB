4eb128c4b2634143512f7b575b24df8779af2d10
danielacmg <dcmg.ucv@gmail.com>
2023-02-13 05:35:18 +0000

    DB Model V1

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+follower = Table(
+    "follower",
+    Base.metadata,
+    Column("follower", ForeignKey("user.id")),
+    Column("following", ForeignKey("user.id")),
+)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    # id = Column(Integer, primary_key=True)
+    id = Column(String(250), primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id")) 
+    post_id = Column(Integer, ForeignKey("post.id"), index=True) 
+    body = Column(String(250), nullable=False)
+    user = relationship("User")
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True, index=True)
+    author_id = Column(Integer, ForeignKey("user.id")) 
+    image = Column(String(300))
+    date = Column(String(300))
+
+    # Relationships
+    author = relationship("User")
+    comments = relationship("Comment")
+
+
 
     def to_dict(self):
         return {}
